c2ac9d9d69937492c5ecb2bbef5a06c6d5ee8477
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-11-12 15:47:10 -0600

    add CPU job script

---
+++ chtc/submit_cpu.sh
@@ -0,0 +1,11 @@
+results_dir=results/${1}
+log_dir=logs/${1}
+commands_file=scripts/${1}
+
+mkdir -p ${results_dir}
+mkdir -p ${log_dir}
+
+condor_submit job_cpu.sub \
+  results_dir=${results_dir} \
+  log_dir=${log_dir} \
+  commands_file=${commands_file}
