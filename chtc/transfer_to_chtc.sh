8d622547ebf0879bd8a605734d0e32a5d10a55da
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-11-14 16:11:47 -0600

    update transfer_to_chtc.sh job script

--- chtc/transfer_to_chtc.sh
+++ chtc/transfer_to_chtc.sh
@@ -2,9 +2,18 @@ f=metaworld-algorithms
 
 cd ../.. # cd just outside the repo
 tar --exclude="chtc" \
+    --exclude=".DS_Store" \
+    --exclude=".gitignore" \
+    --exclude=".venv" \
+    --exclude=".python-version" \
     --exclude=".git" \
-    --exclude="dro_examples/run_results" \
+    --exclude=".idea" \
+    --exclude="metaworld/.gitignore" \
+    --exclude="metaworld/.github" \
+    --exclude="metaworld/.git" \
+    --exclude="metaworld/metaworld.egg-info" \
+    --exclude="metaworld/docs" \
     -czvf ${f}.tar.gz $f
 
-scp ${f}.tar.gz ncorrado@ap2001.chtc.wisc.edu:/staging/ncorrado
+scp ${f}.tar.gz whuang369@ap2001.chtc.wisc.edu:/staging/whuang369
 rm ${f}.tar.gz
