8a9be1bbec759d084556d99ed9c76228dcbeaafc
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-11-14 16:31:42 -0600

    update

--- chtc/transfer_to_chtc.sh
+++ chtc/transfer_to_chtc.sh
@@ -11,6 +11,7 @@ tar --exclude="chtc" \
     --exclude="metaworld/.gitignore" \
     --exclude="metaworld/.github" \
     --exclude="metaworld/.git" \
+    --exclude="metaworld/.idea" \
     --exclude="metaworld/metaworld.egg-info" \
     --exclude="metaworld/docs" \
     -czvf ${f}.tar.gz $f
