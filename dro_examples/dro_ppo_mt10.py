8f14c4c991cabfb1f03c3fc46dca18011aca1a66
Nicholas Corrado <ncorrado@wisc.edu>
2025-11-14 02:12:26 -0600

    change defaults

--- dro_examples/dro_ppo_mt10.py
+++ dro_examples/dro_ppo_mt10.py
@@ -58,7 +58,7 @@ def main() -> None:
             target_kl=None,
             clip_vf_loss=False,
             normalize_advantages=False,
-            dro_upd_num_steps=50_000, # 100 trajectories
+            dro_upd_num_steps=10_000, # this is per environment, so this is 100k steps = 200 trajectories
         ),
         training_config=OnPolicyTrainingConfig(
             total_steps=int(2e7),
